23d0ef399cda06101fa61a3a9cc4b569bc1f977c
Mario Bodom <mariotecladista666@gmail.com>
2022-04-26 10:05:36 +0200

    Solo queda insertar

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String fichero = "E:/DAM/Programacion/Java/Ejercicios iniciales/Practica6_Ficheros_MarioDeSantiago/text.txt";
+        String ficheroEscrito = "E:/DAM/Programacion/Java/Ejercicios iniciales/Practica6_Ficheros_MarioDeSantiago/textoEscrito.txt";
         VectorStrings v = new VectorStrings(fichero);
         System.out.println(v);
 
-        v.escribe("E:/DAM/Programacion/Java/Ejercicios iniciales/Practica6_Ficheros_MarioDeSantiago/textoEscrito.txt");
+        v.escribe(ficheroEscrito);
 
     }
 }
